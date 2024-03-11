6e71cd918fc27c872a0fe414fcdda642d62b9404
ritu-thombre99 <renitath99@gmail.com>
2024-03-11 15:47:43 -0700

    added all required datasets

--- datasets.py
+++ datasets.py
@@ -3,6 +3,10 @@ from pennylane import numpy as np
 
 from sklearn.model_selection import train_test_split
 import numpy
+import sklearn
+from sklearn import datasets
+import matplotlib.pyplot as plt
+from ucimlrepo import fetch_ucirepo 
 
 def get_xor_data(size):
     rng = numpy.random.RandomState(0)
@@ -12,3 +16,45 @@ def get_xor_data(size):
 
     train_X, test_X, train_y, test_y = train_test_split(X, Y, test_size=0.10, random_state=42)
     return train_X, test_X, train_y, test_y
+
+
+def get_moon_dataset(size):
+    moon = sklearn.datasets.make_moons(n_samples=size,noise=0.07)
+    X = np.array(moon[0],requires_grad=False)
+    y = np.array(np.where(moon[1], 1, -1), requires_grad=False)
+    train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.10, random_state=42)
+    return train_X, test_X, train_y, test_y
+
+
+def get_circular_boundary_dataset(size):
+    dataset = sklearn.datasets.make_circles(noise=0.15, factor=0.5)
+    X = np.array(dataset[0],requires_grad=False)
+    y = np.array(np.where(dataset[1], 1, -1), requires_grad=False)
+    train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.10, random_state=42)
+    return train_X, test_X, train_y, test_y
+
+
+def iris_dataset():
+    iris = datasets.load_iris()
+    X = np.array(iris['data'],requires_grad=False)
+    y = np.array(iris['target'],requires_grad=False)
+    y = np.where(y == 0, -2, (np.where(y == 1, 0, 2))) #  reformat labels to -2,0,-2
+    train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.10, random_state=42) 
+    return train_X, test_X, train_y, test_y
+
+
+def get_wine_dataset():
+    wine = fetch_ucirepo(id=109)  
+    X = np.array(wine.data.features ,requires_grad=False)
+    y = np.array(wine.data.targets,requires_grad=False)
+    y = np.where(y == 0, -2, (np.where(y == 1, 0, 2))) #  reformat labels to -2,0,-2
+    # metadata 
+    # print(wine.metadata) 
+    # variable information 
+    # print(wine.variables)
+    train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.10, random_state=42)
+    return train_X, test_X, train_y, test_y
+  
+
+def plot_2d_data(data, labels):
+    plt.scatter(data[:, 0], data[:, 1], c=labels)
