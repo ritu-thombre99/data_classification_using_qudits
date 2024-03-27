92ea2dbb6add368bbbbb5921228493a99619f25b
ritu-thombre99 <renitath99@gmail.com>
2024-03-27 02:36:00 -0700

    trying qutrit with scipy.linalg.expm

--- run_qubit.py
+++ run_qubit.py
@@ -29,8 +29,6 @@ config['encoding_and_rotation_scheme'] = 'B'
 config['s_params_size'] = 1
 config['w_params_size'] = 3
 
-f = open("./logs/qubit_run.txt","a")
-
 
 dev = qml.device("default.qubit", wires=1)
 @qml.qnode(dev)
@@ -112,11 +110,11 @@ def run(dataset='circular', encoding_and_rotation_scheme='B',dataset_size=200,nu
     print("Scheme:",encoding_and_rotation_scheme)
     print("Iters:",num_its)    
 
-    config = {}
     config['dataset'] = dataset
     config['encoding_and_rotation_scheme'] = encoding_and_rotation_scheme
     config['s_params_size'], config['w_params_size'] = scheme_config[encoding_and_rotation_scheme]
 
+    
     if train_X is None:
         if dataset == 'xor':
             train_X, test_X, train_y, test_y = get_xor_data(dataset_size)
